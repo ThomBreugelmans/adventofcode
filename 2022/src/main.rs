ecc311b1f2276ba02426d5f83c1fb07902c9f22a
Thom Breugelmans <thombreugelmans@outlook.com>
2023-11-08 22:12:16 +0100

    added code for day 1

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+use std::process::exit;
+
+mod day1;
+mod day2;
+
+#[derive(Parser)]
+struct CliArguments {
+    day: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArguments::parse();
+    if args.day > 25 {
+        println!("Days until Christmas only run until 25!");
+        exit(1);
+    } else if args.day == 0 {
+        println!("There is no 0th day until Christmas!");
+        exit(1);
+    }
+
+    match args.day {
+        1 => day1::run(),
+        _ => unimplemented!(),
+    };
 }
