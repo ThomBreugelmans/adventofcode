1cf9f3a97a30c9c7475dc1e62ffa5db8aa8aa091
Thom Breugelmans <thombreugelmans@outlook.com>
2023-11-09 17:28:36 +0100

    added advent of code day 5 2022 part 1

---
+++ 2022/src/day5.rs
@@ -0,0 +1,79 @@
+use std::collections::VecDeque;
+use std::fs::read_to_string;
+
+pub fn run() {
+    // get input code:
+    let input: Vec<String> = {
+        let mut _input = Vec::new();
+        for line in read_to_string("input/day5").unwrap().lines() {
+            _input.push(line.to_string());
+        }
+        _input
+    };
+
+    let output = _run(input);
+    println!("{}", output);
+}
+
+fn parse(input: Vec<String>) -> (Vec<VecDeque<char>>, Vec<(i32, i32, i32)>) {
+    let mut stacks = Vec::<VecDeque<char>>::new();
+    for _ in 0..((input[0].len() + 1) / 4) {
+        stacks.push(VecDeque::<char>::new());
+    }
+    let mut input_iter = input.iter();
+    let mut row = input_iter.next().unwrap();
+    while !row.is_empty() {
+        let mut iterator = row.chars();
+        let _ = iterator.next();
+        for i in 0..stacks.len() {
+            let b = iterator.next().unwrap();
+            let _ = iterator.nth(2);
+            if b != ' ' {
+                stacks[i].push_front(b);
+            }
+        }
+        row = input_iter.next().unwrap();
+    }
+
+    // stacks have been created, now parse actions
+    let mut actions = Vec::new();
+    for row in input_iter {
+        if row.is_empty() {
+            continue;
+        }
+        let x = row.split(' ').collect::<Vec<&str>>();
+        actions.push((
+            x[1].parse::<i32>().unwrap(),
+            x[3].parse::<i32>().unwrap() - 1,
+            x[5].parse::<i32>().unwrap() - 1,
+        ));
+    }
+    (stacks, actions)
+}
+
+fn _run(input: Vec<String>) -> String {
+    let (mut stacks, actions) = parse(input);
+
+    for (count, from, to) in actions {
+        for _ in 0..count {
+            let b = stacks[from as usize].pop_back().unwrap();
+            stacks[to as usize].push_back(b);
+        }
+    }
+
+    String::from_iter(stacks.iter().map(|e| e.clone().pop_back().unwrap()))
+}
+
+#[test]
+fn test() {
+    let mut answer = "".to_string();
+    let mut input = Vec::<String>::new();
+    for line in read_to_string("input/test/day5").unwrap().lines() {
+        if answer.is_empty() {
+            answer = line.to_string();
+            continue;
+        }
+        input.push(line.to_string());
+    }
+    assert_eq!(answer, _run(input));
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -5,6 +5,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 #[derive(Parser)]
 struct CliArguments {
@@ -26,6 +27,7 @@ fn main() {
         2 => day2::run(),
         3 => day3::run(),
         4 => day4::run(),
+        5 => day5::run(),
         _ => unimplemented!(),
     };
 }
