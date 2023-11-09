748eb2620be857f53371d2cb03b769299493ca64
Thom Breugelmans <thombreugelmans@outlook.com>
2023-11-09 18:25:11 +0100

    added advent of code day 6 2022 part 1

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -6,6 +6,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 #[derive(Parser)]
 struct CliArguments {
@@ -28,6 +29,7 @@ fn main() {
         3 => day3::run(),
         4 => day4::run(),
         5 => day5::run(),
+        6 => day6::run(),
         _ => unimplemented!(),
     };
 }
