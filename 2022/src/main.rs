a1c8a49f9cab86022f6d861f5ee5c79f3fdd8d42
Thom Breugelmans <thombreugelmans@outlook.com>
2023-11-14 00:17:26 +0100

    added day 14 2022 part 1

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -9,6 +9,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -59,6 +60,7 @@ fn main() {
         11 => day11::run(input),
         12 => day12::run(input),
         13 => day13::run(input),
+        14 => day14::run(input),
         _ => unimplemented!(),
     };
 
