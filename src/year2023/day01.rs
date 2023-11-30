a609003b38ac2fb7f13cfde271e475a8ab1957a1
Thom Breugelmans <thombreugelmans@outlook.com>
2023-11-30 23:41:57 +0100

    added basis for aoc2023

---
+++ src/year2023/day01.rs
@@ -0,0 +1,17 @@
+fn parse(input: &str) -> () {}
+
+pub fn run(input: &str) -> String {
+    format!("Part 1: {}\nPart 2: {}", run_part1(input), "")
+}
+
+fn run_part1(input: &str) -> String {
+    todo!()
+}
+
+const TEST_INPUT: &str = "";
+
+#[test]
+fn test_part1() {
+    let answer = "";
+    assert_eq!(answer, run_part1(TEST_INPUT));
+}
