d910879613671766961d368a7750aafc7dbd66a1
Thom Breugelmans <thombreugelmans@outlook.com>
2023-12-02 07:53:19 +0100

    added code for 2023 day 2
    modified the mod.rs, lib.rs to also point to the new day and added
    day02.rs and day02 for the code and the input

--- src/lib.rs
+++ src/lib.rs
@@ -28,4 +28,5 @@ pub mod year2022 {
 
 pub mod year2023 {
     pub mod day01;
+    pub mod day02;
 }
---
+++ src/year2023/day02.rs
@@ -0,0 +1,120 @@
+#[derive(Debug, PartialEq, Eq)]
+struct Game {
+    id: usize,
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+fn parse(input: &str) -> Vec<Game> {
+    let mut games = Vec::new();
+    for line in input.lines() {
+        let mut game = Game {
+            id: games.len() + 1,
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+        let data = line
+            .trim()
+            .split([':', ';', ','])
+            .map(|section| section.trim());
+        for d in data {
+            let temp = d.split(' ').collect::<Vec<&str>>();
+            match (temp[0], temp[1]) {
+                (_, "red") => game.red = game.red.max(temp[0].parse::<u32>().unwrap()),
+                (_, "green") => game.green = game.green.max(temp[0].parse::<u32>().unwrap()),
+                (_, "blue") => game.blue = game.blue.max(temp[0].parse::<u32>().unwrap()),
+                _ => (),
+            }
+        }
+        games.push(game);
+    }
+    games
+}
+
+pub fn run(input: &str) -> String {
+    format!("Part 1: {}\nPart 2: {}", run_part1(input), run_part2(input))
+}
+
+fn run_part1(input: &str) -> String {
+    let games = parse(input);
+    const max_red: u32 = 12;
+    const max_green: u32 = 13;
+    const max_blue: u32 = 14;
+    games
+        .into_iter()
+        .filter_map(|game| {
+            if game.red > max_red || game.green > max_green || game.blue > max_blue {
+                None
+            } else {
+                Some(game.id as u32)
+            }
+        })
+        .sum::<u32>()
+        .to_string()
+}
+
+fn run_part2(input: &str) -> String {
+    let games = parse(input);
+    games
+        .into_iter()
+        .map(|game| game.red * game.blue * game.green)
+        .sum::<u32>()
+        .to_string()
+}
+
+const TEST_INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+#[test]
+fn test_parsing() {
+    let answer = vec![
+        Game {
+            id: 1,
+            red: 4,
+            green: 2,
+            blue: 6,
+        },
+        Game {
+            id: 2,
+            red: 1,
+            green: 3,
+            blue: 4,
+        },
+        Game {
+            id: 3,
+            red: 20,
+            green: 13,
+            blue: 6,
+        },
+        Game {
+            id: 4,
+            red: 14,
+            green: 3,
+            blue: 15,
+        },
+        Game {
+            id: 5,
+            red: 6,
+            green: 3,
+            blue: 2,
+        },
+    ];
+    assert_eq!(answer, parse(TEST_INPUT));
+}
+
+#[test]
+fn test_part1() {
+    let answer = "8";
+    assert_eq!(answer, run_part1(TEST_INPUT));
+}
+
+#[test]
+fn test_part2() {
+    let answer = "2286";
+    assert_eq!(answer, run_part2(TEST_INPUT));
+}
