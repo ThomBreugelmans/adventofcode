0279a99c1cdb5fcef0914b32cfb62dc6f717e909
Thom Breugelmans <thombreugelmans@outlook.com>
2023-12-02 07:53:19 +0100

    added code for 2023 day 2
    modified the mod.rs, lib.rs to also point to the new day and added
    day02.rs and day02 for the code and the input

--- src/year2023/mod.rs
+++ src/year2023/mod.rs
@@ -4,6 +4,7 @@ use aoc::utils::solution::*;
 pub fn run(day: u32) -> Solution {
     match day {
         1 => solution!(year2023, day01),
+        2 => solution!(year2023, day02),
         _ => panic!("[E] Unknown day..."),
     }
 }
