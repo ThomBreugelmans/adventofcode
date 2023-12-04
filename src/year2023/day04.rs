11921d32134b6f5fb4975b5e45ca96e02e80cb48
Thom Breugelmans <thombreugelmans@outlook.com>
2023-12-04 12:50:34 +0100

    added code for 2023 day 4

---
+++ src/year2023/day04.rs
@@ -0,0 +1,104 @@
+use std::collections::VecDeque;
+
+use nom::bytes::complete::tag;
+use nom::character::complete::{digit1, space0, space1};
+use nom::multi::separated_list1;
+use nom::sequence::{preceded, separated_pair};
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct ScratchCard {
+    id: usize,
+    scratched_numbers: Vec<u32>,
+    winning_numbers: Vec<u32>,
+}
+
+fn parse(input: &str) -> Vec<ScratchCard> {
+    let mut cards = Vec::new();
+    for (id, line) in input.lines().enumerate() {
+        let card = line.split(':').collect::<Vec<&str>>()[1];
+        match separated_pair(
+            preceded(
+                space0::<&str, nom::error::Error<&str>>,
+                separated_list1(space1, digit1),
+            ),
+            tag(" | "),
+            preceded(space0, separated_list1(space1, digit1)),
+        )(card)
+        {
+            Ok(("", (scratched, winning))) => cards.push(ScratchCard {
+                id: id + 1,
+                scratched_numbers: scratched
+                    .into_iter()
+                    .map(|c| c.parse::<u32>().unwrap())
+                    .collect(),
+                winning_numbers: winning
+                    .into_iter()
+                    .map(|c| c.parse::<u32>().unwrap())
+                    .collect(),
+            }),
+            _ => (),
+        }
+    }
+    cards
+}
+
+fn number_of_matches(s: &ScratchCard) -> usize {
+    s.scratched_numbers
+        .iter()
+        .filter(|n| s.winning_numbers.contains(n))
+        .count()
+}
+
+pub fn run(input: &str) -> String {
+    format!("Part 1: {}\nPart 2: {}", run_part1(input), run_part2(input))
+}
+
+fn run_part1(input: &str) -> String {
+    let scratchcards = parse(input);
+    scratchcards
+        .into_iter()
+        .filter_map(|s| {
+            let x = number_of_matches(&s);
+            if x > 0 {
+                Some(2u32.pow(x.saturating_sub(1) as u32))
+            } else {
+                None
+            }
+        })
+        .sum::<u32>()
+        .to_string()
+}
+
+fn run_part2(input: &str) -> String {
+    let scratchcards = parse(input);
+    let mut scratched = 0u32;
+    let mut frontier = VecDeque::new();
+    scratchcards.iter().for_each(|s| frontier.push_back(s));
+    while let Some(card) = frontier.pop_front() {
+        scratched += 1;
+        let x = number_of_matches(card);
+        for new in (card.id)..(card.id + x) {
+            frontier.push_back(&scratchcards[new]);
+        }
+    }
+    scratched.to_string()
+}
+
+const TEST_INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+#[test]
+fn test_part1() {
+    let answer = "13";
+    assert_eq!(answer, run_part1(TEST_INPUT));
+}
+
+#[test]
+fn test_part2() {
+    let answer = "30";
+    assert_eq!(answer, run_part2(TEST_INPUT));
+}
--- src/year2023/mod.rs
+++ src/year2023/mod.rs
@@ -6,6 +6,7 @@ pub fn run(day: u32) -> Solution {
         1 => solution!(year2023, day01),
         2 => solution!(year2023, day02),
         3 => solution!(year2023, day03),
+        4 => solution!(year2023, day04),
         _ => panic!("[E] Unknown day..."),
     }
 }
