a82f9f8fff4f2b5b86b74a55696dc0443beb03c0
Thom Breugelmans <thombreugelmans@outlook.com>
2023-12-05 11:08:36 +0100

    added code for 2023 day 5
    code will be cleaned up a bit because the time it takes to compute
    part 2 is enormously slow

--- src/year2023/mod.rs
+++ src/year2023/mod.rs
@@ -7,6 +7,7 @@ pub fn run(day: u32) -> Solution {
         2 => solution!(year2023, day02),
         3 => solution!(year2023, day03),
         4 => solution!(year2023, day04),
+        5 => solution!(year2023, day05),
         _ => panic!("[E] Unknown day..."),
     }
 }
