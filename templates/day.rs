5dc09ef6626f390ca6943ecabf6f0b0056e39d8e
Thom Breugelmans <thombreugelmans@outlook.com>
2023-12-08 11:05:37 +0100

    added templates to more easily add days and years

---
+++ templates/day.rs
@@ -0,0 +1,30 @@
+fn parse(input: &str) -> () {
+    todo!()
+}
+
+pub fn run(input: &str) -> String {
+    todo!()
+}
+
+fn run_part1(input: &str) -> String {
+    todo!()
+}
+
+fn run_part2(input: &str) -> String {
+    todo!()
+}
+
+#[allow(dead_code)]
+const TEST_INPUT: &str = "";
+
+#[test]
+fn test_part1() {
+    let answer = "";
+    assert_eq!(answer, run_part1(TEST_INPUT));
+}
+
+#[test]
+fn test_part2() {
+    let answer = "";
+    assert_eq!(answer, run_part2(TEST_INPUT));
+}
