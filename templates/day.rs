f5995f88737306ea283ced6df584afe8fc1973fa
Thom Breugelmans <thombreugelmans@outlook.com>
2025-12-04 20:58:12 +0100

    fix(templates): update templates

--- templates/day.rs
+++ templates/day.rs
@@ -4,14 +4,14 @@ fn parse(input: &str) -> Vec<&str> {
     input.trim().split('\n').map(|x| x).collect()
 }
 
-#[solution(year=202x, day=x, part=1)]
+// #[solution(year=202x, day=x, part=1)]
 fn part1(input: &str) -> String {
     let parsed = parse(input);
     let mut sum = 0u64;
     sum.to_string()
 }
 
-#[solution(year=202x, day=x, part=2)]
+// #[solution(year=202x, day=x, part=2)]
 fn part2(input: &str) -> String {
     let parsed = parse(input);
     let mut sum = 0u64;
