42c31e2f31402310a85360b96058994ee4cb4193
Thom Breugelmans <thombreugelmans@outlook.com>
2025-12-05 08:11:56 +0100

    fix(2025-4): compiler warning about unused variable

--- src/year2025/day04.rs
+++ src/year2025/day04.rs
@@ -92,7 +92,6 @@ fn get_movable_paperrolls(
 ) -> Vec<(usize, usize)> {
     let mut movable = vec![];
     for y in 0..boundary.1 {
-        let row = &grid[y];
         for x in 0..boundary.0 {
             is_movable(grid, x, y).then(|| movable.push((x, y)));
         }
