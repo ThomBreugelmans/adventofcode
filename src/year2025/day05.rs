fceb67dd85d4e54fdca922246a445180a285ba65
Thom Breugelmans <thombreugelmans@outlook.com>
2025-12-05 09:08:05 +0100

    refactor(2025-5): removal of overlap moved for seperate function

--- src/year2025/day05.rs
+++ src/year2025/day05.rs
@@ -1,6 +1,29 @@
 use itertools::Itertools;
 use macros::solution;
 
+fn remove_overlapping(ranges: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    let mut total_ranges = Vec::new();
+    for (a, b) in ranges {
+        let colliding_ranges = total_ranges
+            .iter()
+            .enumerate()
+            .filter(|(_, (c, d))| !(a > *d || b < *c))
+            .map(|(i, (c, d))| (i, (*c, *d)))
+            .collect_vec();
+        if colliding_ranges.is_empty() {
+            total_ranges.push((a, b));
+            continue;
+        }
+        for i in colliding_ranges.iter().rev().map(|(i, _)| i) {
+            total_ranges.remove(*i);
+        }
+        let min = a.min(colliding_ranges.iter().map(|(_, (a, _))| *a).min().unwrap());
+        let max = b.max(colliding_ranges.iter().map(|(_, (_, b))| *b).max().unwrap());
+        total_ranges.push((min, max));
+    }
+    total_ranges
+}
+
 fn parse(input: &str) -> (Vec<(i64, i64)>, Vec<i64>) {
     let mut parts = input.trim().split("\n\n");
     let fresh_ranges = parts
@@ -21,7 +44,7 @@ fn parse(input: &str) -> (Vec<(i64, i64)>, Vec<i64>) {
         .split('\n')
         .map(|x| x.parse::<i64>().unwrap())
         .collect_vec();
-    (fresh_ranges, ingredient_ids)
+    (remove_overlapping(fresh_ranges), ingredient_ids)
 }
 
 #[solution(year = 2025, day = 5, part = 1)]
@@ -42,30 +65,9 @@ fn part1(input: &str) -> String {
 
 #[solution(year = 2025, day = 5, part = 2)]
 fn part2(input: &str) -> String {
+    // fresh_ranges is already de-overlapped
     let (fresh_ranges, _) = parse(input);
-    let mut total_ranges = Vec::new();
-    for (a, b) in fresh_ranges {
-        let colliding_ranges = total_ranges
-            .iter()
-            .enumerate()
-            .filter(|(_, (c, d))| !(a > *d || b < *c))
-            .map(|(i, (c, d))| (i, (*c, *d)))
-            .collect_vec();
-        // println!("({a},{b})");
-        // println!("{:?} {:?}", colliding_ranges, total_ranges);
-        if colliding_ranges.is_empty() {
-            total_ranges.push((a, b));
-            continue;
-        }
-        for i in colliding_ranges.iter().rev().map(|(i, _)| i) {
-            total_ranges.remove(*i);
-        }
-        let min = a.min(colliding_ranges.iter().map(|(_, (a, _))| *a).min().unwrap());
-        let max = b.max(colliding_ranges.iter().map(|(_, (_, b))| *b).max().unwrap());
-        total_ranges.push((min, max));
-    }
-    // println!("\n\n{:?}", total_ranges);
-    total_ranges
+    fresh_ranges
         .iter()
         .map(|(a, b)| b - a + 1)
         .sum::<i64>()
