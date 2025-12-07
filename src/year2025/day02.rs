6e59fa49860c5d5a40ad7f48b2c4340947825dce
Thom Breugelmans <thombreugelmans@outlook.com>
2025-12-07 15:54:35 +0100

    refactor(2025-2): change f64::log10 to i64::ilog10
    changed to make use of ilog, which gave a speedup of 3~5x

--- src/year2025/day02.rs
+++ src/year2025/day02.rs
@@ -19,13 +19,13 @@ fn run_part1(input: &str) -> String {
     let parsed = parse(input);
     let mut sum = 0;
     for (a, b) in parsed {
-        let char_count_a = f64::log10(a as f64) as u32 + 1;
+        let char_count_a = i64::ilog10(a) as u32 + 1;
         let half_pow_a = 10i64.pow(char_count_a / 2 + if char_count_a % 2 == 0 { 0 } else { 1 });
-        let half_pow_b = 10i64.pow((f64::log10(b as f64) as u32 + 1) / 2);
-        let n1 = a / half_pow_a;
+        let half_pow_b = 10i64.pow((i64::ilog10(b) as u32 + 1) / 2);
+        let n1 = (a / half_pow_a).max(1);
         let n2 = b / half_pow_b;
         for n_ in n1..=n2 {
-            let pow = f64::log10(n_ as f64) as u32 + 1;
+            let pow = i64::ilog10(n_) as u32 + 1;
             let n = n_ * 10i64.pow(pow) + n_;
             if (a..=b).contains(&n) {
                 sum += n;
@@ -40,12 +40,12 @@ fn run_part2(input: &str) -> String {
     let parsed = parse(input);
     let mut found = Vec::new();
     for (a, b) in parsed {
-        let char_count_a = f64::log10(a as f64) as u32 + 1;
-        let char_count_b = f64::log10(b as f64) as u32 + 1;
+        let char_count_a = i64::ilog10(a) as u32 + 1;
+        let char_count_b = i64::ilog10(b) as u32 + 1;
         let start = (a / (10i64.pow(char_count_a))).max(1);
         let end = b / 10i64.pow(char_count_b / 2);
         for n in start..=end {
-            let pow_n = 10i64.pow(f64::log10(n as f64) as u32 + 1);
+            let pow_n = 10i64.pow(i64::ilog10(n) as u32 + 1);
             let mut m = n;
             while m < a {
                 m = m * pow_n + n;
